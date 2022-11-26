4ab6689cae705b3b6b5fd9661aec461f433bd794
iron <iron.cheng@qq.com>
2022-11-27 01:24:31 +0800

    fix: null 2 json warning

--- api4go/main.go
+++ api4go/main.go
@@ -13,21 +13,21 @@ import (
 )
 
 type Dict struct {
-	Id          int    `json:"id"`
-	Word        string `json:"word"`
-	Sw          string `json:"sw"`
-	Phonetic    string `json:"phonetic"`
-	Definition  string `json:"definition"`
-	Translation string `json:"translation"`
-	Pos         string `json:"pos"`
-	Collins     int    `json:"collins"`
-	Oxford      int    `json:"xxford"`
-	Tag         string `json:"tag"`
-	Bnc         int    `json:"bnc"`
-	Frq         int    `json:"frq"`
-	Exchange    string `json:"exchange"`
-	Detail      string `json:"detail"`
-	Audio       string `json:"audio"`
+	Id          int     `json:"id"`
+	Word        string  `json:"word"`
+	Sw          string  `json:"sw"`
+	Phonetic    string  `json:"phonetic"`
+	Definition  string  `json:"definition"`
+	Translation string  `json:"translation"`
+	Pos         string  `json:"pos"`
+	Collins     *int    `json:"collins"`
+	Oxford      *int    `json:"oxford"`
+	Tag         string  `json:"tag"`
+	Bnc         int     `json:"bnc"`
+	Frq         int     `json:"frq"`
+	Exchange    string  `json:"exchange"`
+	Detail      *string `json:"detail"`
+	Audio       string  `json:"audio"`
 }
 
 var stmt *sql.Stmt
@@ -44,17 +44,23 @@ func initDB(dbPath string) {
 }
 
 func dictHandler(w http.ResponseWriter, r *http.Request) {
-	q := r.URL.Query()
-	result := queryWord(q.Get("q"))
-	json.NewEncoder(w).Encode(result)
+	query := r.URL.Query()
+	q := query.Get("q")
+	if q != "" {
+		result, err := queryWord(q)
+		if err == nil {
+			json.NewEncoder(w).Encode(result)
+		}
+	}
 }
 
-func queryWord(q string) (dict Dict) {
+func queryWord(q string) (dict Dict, err error) {
 	row := stmt.QueryRow(q)
-	row.Scan(&dict.Id, &dict.Word, &dict.Sw, &dict.Phonetic, &dict.Definition,
+	err = row.Scan(&dict.Id, &dict.Word, &dict.Sw, &dict.Phonetic, &dict.Definition,
 		&dict.Translation, &dict.Pos, &dict.Collins, &dict.Oxford, &dict.Tag,
 		&dict.Bnc, &dict.Frq, &dict.Exchange, &dict.Detail, &dict.Audio,
 	)
+
 	return
 }
 
