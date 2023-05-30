de6f36666d8301ebf588a8123cfe17d2603a0a10
iron <iron.cheng@qq.com>
2023-05-31 00:29:18 +0800

    refactor(deno): use npm pkg by CDN (deploy does not support npm specifiers now)

--- api4deno/deps.ts
+++ api4deno/deps.ts
@@ -1,2 +1,2 @@
-export { serve } from "https://deno.land/std@0.188.0/http/mod.ts";
-export { connect } from "npm:@planetscale/database@^1.7.0";
+export { serve } from "https://deno.land/std@0.188.0/http/mod.ts"; 
+export { connect } from "https://esm.sh/@planetscale/database@1.7.0";
