707153eefb2b1707f0e6b7192c07cdef7b656a01
heng1025 <iron.cheng@qq.com>
2023-07-13 22:36:27 +0800

    feat(deno user): use md5 handling for password

--- api4deno/deps.ts
+++ api4deno/deps.ts
@@ -1,6 +1,10 @@
-export { serve } from "https://deno.land/std@0.188.0/http/mod.ts";
-export { format } from "https://deno.land/std@0.192.0/datetime/format.ts";
-export { generate } from "https://deno.land/std@0.192.0/uuid/v5.ts";
-export { NAMESPACE_URL } from "https://deno.land/std@0.192.0/uuid/constants.ts";
+export { serve } from "https://deno.land/std@0.194.0/http/mod.ts";
+export { format } from "https://deno.land/std@0.194.0/datetime/format.ts";
+export { generate } from "https://deno.land/std@0.194.0/uuid/v5.ts";
+export {
+  crypto,
+  toHashString,
+} from "https://deno.land/std@0.194.0/crypto/mod.ts";
+export { NAMESPACE_URL } from "https://deno.land/std@0.194.0/uuid/constants.ts";
 export { connect } from "https://esm.sh/@planetscale/database@1.7.0";
 export * as jose from "https://deno.land/x/jose@v4.14.4/index.ts";
--- api4deno/main.ts
+++ api4deno/main.ts
@@ -1,6 +1,14 @@
-import "https://deno.land/std@0.188.0/dotenv/load.ts";
+import "https://deno.land/std@0.194.0/dotenv/load.ts";
 
-import { jose, serve, format, generate, NAMESPACE_URL } from "./deps.ts";
+import {
+  jose,
+  serve,
+  format,
+  generate,
+  crypto,
+  toHashString,
+  NAMESPACE_URL,
+} from "./deps.ts";
 import { excuteSql } from "./dbHelper.ts";
 import {
   ApiCode,
@@ -29,11 +37,15 @@ const loginHandler = async (req: Request) => {
   if (req.body) {
     const body = await req.json();
     const { username, password } = body;
+    const cryptoPs = await crypto.subtle.digest(
+      "MD5",
+      new TextEncoder().encode(password)
+    );
     const sql = `SELECT name,password,email,profile_image as profileImage FROM users WHERE name = ?`;
     const rows = await excuteSql(sql, [username]);
     const [user] = <User[]>rows;
     const { password: ps, ...rest } = user;
-    if (ps === password) {
+    if (toHashString(cryptoPs) === ps) {
       const alg = "HS256";
       const jwt = await new jose.SignJWT({ username, password })
         .setProtectedHeader({ alg })
@@ -58,7 +70,7 @@ const recordAdd = async (userId: number, payload: Record) => {
   if (rows.length > 0) {
     return `add ${recordType === "2" ? "favorite" : "history"} success`;
   }
-  
+
   const favSql = `INSERT INTO records (id,record_type,text,title,url,favIconUrl,created_at,created_by) VALUES(?,?,?,?,?,?,?,?)`;
   await excuteSql(favSql, [
     uuid.replace(/-/g, "").slice(0, 24),
@@ -77,19 +89,23 @@ const recordHandler = async (req: Request) => {
   const token = req.headers.get("x-token")!;
   const { payload } = await jose.jwtVerify(token, secret);
   const { username, password } = payload as User;
+  const cryptoPs = await crypto.subtle.digest(
+    "MD5",
+    new TextEncoder().encode(password)
+  );
   const userSql = `SELECT id,name,password FROM users WHERE name = ? and password = ?`;
-  const [user] = await excuteSql<User>(userSql, [username, password]);
+  const [user] = await excuteSql<User>(userSql, [username, toHashString(cryptoPs)]);
   const method = req.method.toLocaleLowerCase();
 
   if (method === "post") {
     const body = await req.json();
-    if (req.body && user.id) {
+    if (req.body && user?.id) {
       return recordAdd(user.id, body);
     }
   } else if (method === "get") {
     const { searchParams } = new URL(req.url);
     const recordType = searchParams.get("type");
-    if (recordType && user.id) {
+    if (recordType && user?.id) {
       const recordSql = `SELECT * FROM records WHERE created_by = ? and record_type = ?`;
       const rows = await excuteSql(recordSql, [String(user.id), recordType]);
       return rows;
